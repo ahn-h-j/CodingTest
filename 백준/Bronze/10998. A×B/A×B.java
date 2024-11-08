fc0664af0d8b46a0d7e1b456076ad81f5982e8b2
ahn_h_j <tkgkd246@gmail.com>
2024-11-08 17:09:12 +0900

    [Bronze V] Title: A×B, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[]args){
+        int firstOperand, secondOperand;
+        Scanner scanner = new Scanner(System.in);
+        firstOperand = scanner.nextInt();
+        secondOperand = scanner.nextInt();
+        System.out.println(firstOperand * secondOperand);
+   }
+}
