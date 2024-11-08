9e35a6eebea8f64424fba58dc76132949ffb4844
ahn_h_j <tkgkd246@gmail.com>
2024-11-08 17:23:54 +0900

    [Bronze II] Title: OX퀴즈, Time: 232 ms, Memory: 18028 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[]args){
+        String testResult;
+        char answerOX;
+        int testNumber = 0;
+        int questionNumber = 0;
+        int totalScore = 0;
+        int weightedScore = 1;
+
+        Scanner scanner = new Scanner(System.in);
+        testNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        for(int i = 0; i < testNumber; i++){
+            testResult = scanner.nextLine();
+//            System.out.println(testResult);
+            while(true){
+
+                if(questionNumber == testResult.length()){
+                    System.out.println(totalScore);
+                    questionNumber = 0;
+                    totalScore = 0;
+                    weightedScore = 1;
+                    break;
+                }
+
+                answerOX = testResult.charAt(questionNumber);
+                if (answerOX == 'O') {
+                    totalScore += weightedScore;
+//                    System.out.println(totalScore);
+                    weightedScore++;
+                    questionNumber++;
+                }
+                else if (answerOX == 'X'){
+                    weightedScore = 1;
+                    questionNumber++;
+                }
+            }
+        }
+   }
+}
