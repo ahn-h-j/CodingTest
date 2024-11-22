6c5a27ca1d57c5a2b29d7a1816613b4e5de6d855
ahn_h_j <tkgkd246@gmail.com>
2024-11-22 09:34:51 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 27.68 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -8,7 +8,18 @@ class Solution {
         // key : 약관 종류 / value : today - 유효기간
         Map<String, LocalDate> termsExpiryMap = new HashMap<>();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+        
+        calculateExpiryDates(terms, formatter, termsExpiryMap, today);
+        findExpiredPrivacies(privacies, formatter, termsExpiryMap, answerList);
 
+        int[] answer = new int[answerList.size()];
+        for (int index = 0; index < answerList.size(); index++) {
+            answer[index] = answerList.get(index);
+        }
+        return answer;
+    }
+   
+    public static void calculateExpiryDates(String[] terms, DateTimeFormatter formatter, Map<String, LocalDate> termsExpiryMap, String today) {
         for (int index = 0; index < terms.length; index++) {
             // today에서 유효기간을 뺀 기간을 구해서 맵에 넣음
             LocalDate date = LocalDate.parse(today, formatter);
@@ -17,22 +28,17 @@ class Solution {
 
             termsExpiryMap.put(terms[index].split(" ")[0], expireDate);
         }
+    }
+
+    public static void findExpiredPrivacies(String[] privacies, DateTimeFormatter formatter, Map<String, LocalDate> termsExpiryMap, List<Integer> answerList) {
         for (int index = 0; index < privacies.length; index++) {
             String[] input = privacies[index].split(" ");
             LocalDate privacyDate = LocalDate.parse(input[0], formatter);
             String privacyCondition = input[1];
-            LocalDate expiryDate = termsExpiryMap.get(privacyCondition);
 
-            if (expiryDate.compareTo(privacyDate) >= 0) {
+            if (termsExpiryMap.get(privacyCondition).compareTo(privacyDate) >= 0) {
                 answerList.add(index + 1);
             }
         }
-
-        // List<Integer> -> int[] 변환
-        int[] answer = new int[answerList.size()];
-        for (int index = 0; index < answerList.size(); index++) {
-            answer[index] = answerList.get(index);
-        }
-        return answer;
     }
 }
