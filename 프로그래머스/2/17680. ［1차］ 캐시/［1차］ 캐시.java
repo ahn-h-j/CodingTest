6202c09648e7d4ef86eef999d2d937f990120ace
ahn_h_j <tkgkd246@gmail.com>
2024-12-03 11:00:04 +0900

    [level 2] Title: [1차] 캐시, Time: 50.38 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,25 +1,28 @@
 import java.util.ArrayList;
 
-class Solution{ 
+class Solution {
     public int solution(int cacheSize, String[] cities) {
-            int answer = 0;
-            ArrayList<String> inCache = new ArrayList<>();
-            if(cacheSize == 0){
-                return cities.length * 5;
-            }
+        int answer = 0;
+        ArrayList<String> inCache = new ArrayList<>();
+        
+        if(cacheSize == 0){
+            return cities.length * 5;
+        }
 
-            for(String city : cities){
-                city = city.toUpperCase();
+        for(String city : cities){
+            city = city.toUpperCase();
+            if(inCache.contains(city)){
+                inCache.remove(city);
                 inCache.add(city);
-                if(inCache.subList(0,inCache.size() - 1).contains(city)){
-                    inCache.remove(city);
-                    answer += 1;
-                } else if(inCache.size() > cacheSize){
+                answer += 1;
+            } else {
+                if(inCache.size() >= cacheSize){
                     inCache.remove(0);
-                    answer += 5;
                 }
+                inCache.add(city);
+                answer += 5;
             }
-            answer += 5 * cacheSize;
-            return answer;
+        }
+        return answer;
     }
 }
