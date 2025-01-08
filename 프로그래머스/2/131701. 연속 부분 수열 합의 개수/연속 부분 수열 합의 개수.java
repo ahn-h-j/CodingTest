62b00aa8af16fc944b6c8941f7624d299a546c66
Ahn HyeongJun <tkgkd246@gmail.com>
2025-01-08 13:22:45 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 83.03 ms, Memory: 113.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 class Solution {
     public static void main(String[] args) {
@@ -8,34 +6,21 @@ class Solution {
         System.out.println(solution.solution(new int[]{7,9,1,1,4}));
     }
     public int solution(int[] elements) {
-        int answer = 0;
-        ArrayList<Integer> arrayList = new ArrayList<>();
-
+        List<Integer> arrayList = new ArrayList();
         for(int index = 0; index < elements.length; index++) {
             arrayList.add(elements[index]);
         }
 
         int index = 0;
-        for(int dist = 1; dist < elements.length; dist++){
+        for(int dist = 1; dist < elements.length - 1; dist++){
             for(int count = 0; count < elements.length; count++){
-                int operand1 = arrayList.get(index);
-                int operand2 = elements[(index % elements.length + dist) % elements.length];
-                int result = operand1 + operand2;
+                arrayList.add(arrayList.get(index) + elements[(index + dist) % elements.length]);
                 index++;
-                arrayList.add(result);
             }
         }
-        int sum = 0;
-        for(int i = 0; i < elements.length; i++){
-            sum += elements[i];
-        }
-        arrayList.add(sum);
+        arrayList.add(Arrays.stream(elements).sum());
         Set<Integer> set = new HashSet(arrayList);
 
-            // 7 9 1 1 4
-            // 16 10 2 5 11
-            // 17 11 6 12 20
-            // 현 단계의 결과 저장 후 다음 반복시 index + 1의 값을 더해줌
         return set.size();
     }
 }
