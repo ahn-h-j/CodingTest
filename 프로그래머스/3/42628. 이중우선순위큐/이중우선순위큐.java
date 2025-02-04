2c1c6d96ece3417b979975f07a0dc1bbe4f03369
Ahn HyeongJun <tkgkd246@gmail.com>
2025-02-04 13:39:49 +0900

    [level 3] Title: 이중우선순위큐, Time: 74.97 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,13 +1,6 @@
 import java.util.*;
 
 class Solution {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        String[] operations = {"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"};
-        System.out.println(Arrays.toString(solution.solution(operations)));
-        String[] operations2 = {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
-        System.out.println(Arrays.toString(solution.solution(operations2)));
-    }
     public int[] solution(String[] operations) {
         PriorityQueue<Integer> ascendingQueue = new PriorityQueue<>();
         PriorityQueue<Integer> descendingQueue = new PriorityQueue<>(Collections.reverseOrder());
